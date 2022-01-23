4576dd5720db47a50fa8ababa2c934ab476fa655
r20031646 <ramdani.meriem2001@gmail.com>
2022-01-23 15:03:34 +0100

    add methode permute

--- src/TriBulle.java
+++ src/TriBulle.java
@@ -2,23 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TriBulle {
-
-
-
     static void triParBulle(ArrayList<Integer> myList){
             int length =  myList.size();
-            int first;
             for(int i=0; i < length; i++) {
                 for(int j=1; j < (length-i); j++) {
                     if (myList.get(j - 1) > myList.get(j)) {
-
-                        first = myList.get(j-1);
+                        permute(myList,j,j-1);
+                        /* int first = myList.get(j-1);
                         myList.set(j-1,myList.get(j));
-                        myList.set(j, first);
+                        myList.set(j, first);*/
                     }
                 }
-                    }
-                }
+            }
+    }
+
+    static void permute(ArrayList<Integer> list,int i,int j){
+        int first = list.get(i);
+        list.set(i,list.get(j));
+        list.set(j,first);
+    }
+
 
 
 }
--- src/main.java
+++ src/main.java
@@ -1,25 +1,22 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class main {
     public static void main(String args[]) {
 
-        ArrayList<Integer> mylist = new ArrayList<>();
-        int min = 0;
-        int max = 50;
-        int length = 10;
-        mylist= randomGenerator(length,max,min);
-        System.out.println(mylist);
-        TriBulle.triParBulle(mylist);
-        System.out.println(mylist);
-
-
+        ArrayList<Integer> myList;
+        myList = randomGenerator(10,50,0);
+        System.out.println(myList);
+        TriBulle.triParBulle(myList);
+        System.out.println(myList);
     }
+
     public static ArrayList<Integer> randomGenerator(int length, int max , int min ){
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < length; i++) {
-            list.add(i, (int) (Math.random() * (max-min)));
+            //list.add(i, (int) (Math.random() * (max-min)));
+            Random random = new Random();
+            list.add(i,random.nextInt(max - min + 1) + min);
         }
         return list;
     }
