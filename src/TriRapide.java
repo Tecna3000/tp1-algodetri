fa67707b306262e48d1a6b8894d86fee2ae15e83
r20031646 <ramdani.meriem2001@gmail.com>
2022-01-28 16:01:30 +0100

    trirapide debu

--- src/TriRapide.java
+++ src/TriRapide.java
@@ -1,7 +1,23 @@
-/*import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class TriRapide {
-    public static void triRapide(ArrayList<Integer>){
+    public static void triRapide(ArrayList<Integer> list){
+
+
+
 
     }
-}*/
+
+    public static void partition (ArrayList<Integer> list){
+        int pivot = list.get(0);
+        int startbig;
+        int fin_zgrand;
+        int debut_zpivot;
+        int fin_zpivot;
+        int debut_nont;
+        int fin_nont;
+        int debut_zpetit;
+        int fin_zpetit;
+
+    }
+}
