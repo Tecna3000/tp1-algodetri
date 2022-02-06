f464935f84d059b0b5a4f08766ec604d899cf9c8
r20031646 <ramdani.meriem2001@gmail.com>
2022-02-06 21:19:24 +0100

    trirapide reussi

--- src/TriRapide.java
+++ src/TriRapide.java
@@ -1,37 +1,39 @@
 import java.util.ArrayList;
 
 public class TriRapide {
-    public static ArrayList< Integer> triRapide(ArrayList<Integer> list, int smallerPos, int biggerPos){
-        if(smallerPos<biggerPos)
+    public static ArrayList< Integer> triRapide(ArrayList<Integer> list, int start, int end){
+        if(start<end)
         {
-            int pivotPosition =partition(list,smallerPos,biggerPos);
-            triRapide(list,smallerPos,pivotPosition-1);
-            triRapide(list,pivotPosition+1,biggerPos);
+            int pivotPosition =partition(list,start,end,start);
+            triRapide(list,start,pivotPosition-1);
+            triRapide(list,pivotPosition+1,end);
         }
        return list;
     }
 
-    public static int partition (ArrayList<Integer> list, int smallerPos, int biggerPos){
+    public static int partition (ArrayList<Integer> list, int start, int end, int pivot){
 
-        int pivotPos =smallerPos;
-        int i =  1;
-        int n = biggerPos;
-        while(i <= n-1){
+        int  smallerPos = pivot;
+        int cmp = pivot + 1;
+        int biggerPos = end+1;
+        while(cmp<biggerPos){
 
-            if (list.get(i)< list.get(pivotPos)){
-                permute(list,i,pivotPos);
-                i++;
+            if (list.get(cmp)< list.get(pivot)){
+                permute(list,cmp,smallerPos);
+                smallerPos++;
+                cmp++;
+                pivot++;
             }
-            else if(list.get(i) >list.get(pivotPos)){
-                permute(list,i, n-1);
-                n--;
+            else if(list.get(cmp) >list.get(pivot)){
+                permute(list,cmp, biggerPos-1);
+                biggerPos--;
             }
             else{
-                pivotPos++;
-                i++;
+                pivot++;
+                cmp++;
             }
         }
-        return pivotPos;
+        return pivot;
     }
 
     // méthode pour permuter deux éléments dans une liste
